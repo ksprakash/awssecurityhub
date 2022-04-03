5ba66081c2e40396b29ed5749a3f04e2c490f838
SaiPrakash <SaiPrakash.Kokkiligadda@coxautoinc.com>
2022-04-03 13:44:51 -0400

    Added Functionality to lower case in utilities module

--- utilities.py
+++ utilities.py
@@ -5,3 +5,7 @@ def convert_from_str_date(dtstr):
     #date = datetime.strptime(dtstr,"%Y-%m-%dT%H:%M:%S.%fZ").astimezone(tz=pytz.timezone('UTC'))
     date = datetime.strptime(dtstr,"%Y-%m-%dT%H:%M:%S.%fZ")
     return date
+
+def change_name_lower(name: str) -> str:
+    name = name.lower()
+    return name
